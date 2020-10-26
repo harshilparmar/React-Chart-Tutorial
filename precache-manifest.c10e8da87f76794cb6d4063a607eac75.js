d3e790b9934d1b61ceab1351d826b54983dffaff
harshilparmar <harshilparmar80@gmail.com>
2020-10-26 14:59:16 +0530

    Updates

---
+++ precache-manifest.c10e8da87f76794cb6d4063a607eac75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9a29ac04c57b4d35fa61f2868dbf38e",
+    "url": "/React-Chart-Tutorial/index.html"
+  },
+  {
+    "revision": "8bdca93ea70183b3a39e",
+    "url": "/React-Chart-Tutorial/static/css/2.f7bc4ee6.chunk.css"
+  },
+  {
+    "revision": "20ab003cf955a64b2b31",
+    "url": "/React-Chart-Tutorial/static/css/main.3a57319c.chunk.css"
+  },
+  {
+    "revision": "8bdca93ea70183b3a39e",
+    "url": "/React-Chart-Tutorial/static/js/2.03b43faf.chunk.js"
+  },
+  {
+    "revision": "9f3e154b5a21336fa161f9ed4e97157c",
+    "url": "/React-Chart-Tutorial/static/js/2.03b43faf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20ab003cf955a64b2b31",
+    "url": "/React-Chart-Tutorial/static/js/main.f4761c76.chunk.js"
+  },
+  {
+    "revision": "24106eda6556b695d3d5",
+    "url": "/React-Chart-Tutorial/static/js/runtime-main.fdbdcd73.js"
+  }
+]);
